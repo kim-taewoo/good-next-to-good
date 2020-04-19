20b5cc4be9684c69e11e9d798b63c82ee3e395b5
kim-taewoo <acoustic0419@gmail.com>
2020-04-20 03:02:04 +0900

    preparing deploy

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  // <React.StrictMode>
+  <Fragment>
+    <App />
+  </Fragment>,
+  // </React.StrictMode>,
+  document.getElementById('root')
+);
